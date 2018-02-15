5af123ba107af10e64f842349dd904d8a568b490
Luis Jose Marquez Gonzalez <ljmarquezg@gmail.com>
2018-02-15 14:30:49 -0400

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  agregarFavoritos(){
+    alert('Agregando')
+  }
+
+  eliminarFavoritos(){
+    alert('Eliminado')
+  }
+
+  agregarComentario(){
+
+  }
+}
